f8312bcf740aa88a0eddf5ee35b045e1da5bab3b
gabriel04rocha <gabriel04rocha@gmail.com>
2026-03-18 15:24:05 -0300

    odd numbers between repetitive structures (for) exercise completed

---
+++ Exercícios/Estruturas repetitivas/Estrutura For/exercicio-impares/src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um valor: ");
+
+        int number = sc.nextInt();
+
+        for (int i = 0; i <= number; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+        
+        sc.close();
+
+    }
+}
