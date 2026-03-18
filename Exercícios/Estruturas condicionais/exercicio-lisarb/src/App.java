80c3fc7f2a8b750efb3eef8cb667fc09a9064959
Gabriel Rocha <gabriel.a.rocha@iesb.edu.br>
2026-03-17 22:56:22 -0300

    exercício de imposto de renda completo

--- Exercícios/Estruturas condicionais/exercicio-lisarb/src/App.java
+++ Exercícios/Estruturas condicionais/exercicio-lisarb/src/App.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite aqui o valor do seu salário usando vírgula: ");
+
+        double salary = sc.nextDouble();
+
+        double tax = 0;
+
+        if (salary < 2000.00) {
+            tax = 0;
+        } else if (salary < 3000.00) {
+            tax = (salary - 2000.00) * 0.08;
+        } else if (salary < 4500.00 && salary > 3000.00) {
+            tax = (999.99 * 0.08) + ((salary - 3000.01) * 0.18);
+        } else if (salary > 4500.00) {
+            tax = (999.99 * 0.08) + (1499.99 * 0.18) + ((salary - 4500) * 0.28);
+        }
+
+        System.out.printf("O valor do imposto é %.2f.", tax);
+        
+        sc.close();
+
     }
 }
