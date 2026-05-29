595b3c1bc0fd373a4331cdb514a9df8f42159353
gabriel04rocha <gabriel04rocha@gmail.com>
2026-05-28 23:03:57 -0300

    chore: Department class created

--- Exercícios/Acesso à banco de dados MySQL/demo-dao-jdbc/src/model/entities/Department.java
+++ Exercícios/Acesso à banco de dados MySQL/demo-dao-jdbc/src/model/entities/Department.java
@@ -1,4 +1,54 @@
 package model.entities;
 
-public class Department {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+
+    private Integer Id;
+    private String Name;
+
+    public Department() {
+
+    }
+
+    public Department(Integer id, String name) {
+        Id = id;
+        Name = name;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Department that)) return false;
+        return Objects.equals(Id, that.Id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(Id);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "Id=" + Id +
+                ", Name='" + Name + '\'' +
+                '}';
+    }
 }
